ef3703acc322a9c709ab7ac1acc8201626090ea6
dapetoo <dapetoo@gmail.com>
2022-12-02 16:14:01 +0100

    PHP-Todo

--- create_user.sql
+++ create_user.sql
@@ -4,3 +4,10 @@ CREATE USER ''@'%' IDENTIFIED BY ''; GRANT ALL PRIVILEGES ON * . * TO ''@'%';
 -- FLUSH PRIVILEGES;
 
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'dapetoo';
+
+CREATE USER 'dapetoo'@'172.18.0.3' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'dapetoo'@'172.18.0.3' WITH GRANT OPTION;
+
+Access denied for user 'dapetoo'@'172.18.0.3' (using password: YES)
+
+GRANT ALL PRIVILEGES ON *.* TO 'dapetoo'@'localhost' WITH GRANT OPTION;
